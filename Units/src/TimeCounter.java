e9cf9e0363d3fd92163015fc7944b36783c62224
David <podrazad@gmail.com>
2025-09-25 20:05:33 +0200

    Add new exercises to repository for training purposes

---
+++ Units/src/TimeCounter.java
@@ -0,0 +1,11 @@
+public class TimeCounter {
+    int hourToMinutes(int a){
+        return a*60;
+    }
+    int minutesToSeconds(int a){
+        return a*60;
+    }
+    int secoundsToMiliseconds(int a){
+        return a*1000;
+    }
+}
