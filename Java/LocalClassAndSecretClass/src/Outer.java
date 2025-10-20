c97aba16bfcab25c42fd2b275bbb627fb50f6c0a
Dawid Podraza <podrazad@gmail.com>
2025-10-20 21:31:52 +0200

    Add some anonymous and local classes

---
+++ Java/LocalClassAndSecretClass/src/Outer.java
@@ -0,0 +1,18 @@
+public class Outer {
+
+    void doSomething(){
+        int x =0;
+
+        class Inner{
+           public void printInfo(){
+                System.out.println(x);
+            }
+
+
+
+        }
+
+    }
+
+
+}
