a31fb1962423c612445b9bbdd339b3f74a3f914c
Dawid Podraza <podrazad@gmail.com>
2025-10-22 21:22:07 +0200

    Add extra file to test branch

--- Java/FileSaveAndRead/src/Main.java
+++ Java/FileSaveAndRead/src/Main.java
@@ -1,19 +1,15 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     static void main(String[] args) throws IOException {
         String fileName = "txtFile";
-        File file = new File(fileName);
-        file.createNewFile();
+        FileReader fileReader = new FileReader(fileName);
+        BufferedReader reader = new BufferedReader(fileReader);
 
 
-        Scanner scanner= new Scanner(file);
-
-        while(scanner.hasNextLine()) {
-            System.out.println(scanner.nextLine());
-
-        }
     }
 }
