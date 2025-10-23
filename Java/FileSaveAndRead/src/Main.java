39c7db077b2c36dbecf5c91973742fb1cf7a140c
Dawid Podraza <podrazad@gmail.com>
2025-10-23 22:17:51 +0200

    Some files write and read exercise

--- Java/FileSaveAndRead/src/Main.java
+++ Java/FileSaveAndRead/src/Main.java
@@ -1,15 +1,26 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
     static void main(String[] args) throws IOException {
         String fileName = "txtFile";
+
+        BufferedWriter fileWriter = new BufferedWriter(new FileWriter(fileName));
+        fileWriter.write("Ania");
+        fileWriter.newLine();
+        fileWriter.write("Kaisa");
+        fileWriter.newLine();
+        fileWriter.write("Basia");
+        fileWriter.close();
+
         FileReader fileReader = new FileReader(fileName);
         BufferedReader reader = new BufferedReader(fileReader);
 
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+        reader.close();
 
     }
 }
