b71f09e4db570255d0cb0d66d4880c687195d16f
Dawid Podraza <podrazad@gmail.com>
2025-11-10 19:58:53 +0100

    Add comments to projects Lotto

--- Java/LottoProject/src/LottoApp.java
+++ Java/LottoProject/src/LottoApp.java
@@ -2,6 +2,8 @@ public class LottoApp {
     static void main(String[] args) {
         Lotto lotto1 = new Lotto();
 
+        // This is lotto application try it !
+
         lotto1.generateList();
         lotto1.randomize();
         lotto1.addFirstSixNumbersToNewList();
