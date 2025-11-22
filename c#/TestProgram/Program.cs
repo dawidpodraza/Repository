4b306b8392207cdecd526f084d7cdc7fad2c18ce
Dawid Podraza <podrazad@gmail.com>
2025-11-22 20:49:40 +0100

    Add first test app

---
+++ c#/TestProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TestProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj pierwszą liczbę: ");
+            int first = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj drugą liczbę: ");
+            int second = int.Parse(Console.ReadLine());
+            int sum = first + second;
+            Console.WriteLine("Suma liczb "+first+", "+second+" wynosi "+sum);
+
+
+        }
+    }
+}
