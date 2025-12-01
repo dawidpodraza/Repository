04080bdc3afaaa1b8594f6365743b6396f54b9e3
Dawid Podraza <podrazad@gmail.com>
2025-12-01 20:32:32 +0100

    Add mail app

--- Java/SendMailTraining/src/Program.java
+++ Java/SendMailTraining/src/Program.java
@@ -1,5 +1,22 @@
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
 public class Program {
-    static void main(String[] args) {
+    static void main(String[] args) throws EmailException {
+        Email email = new SimpleEmail();
+        email.setHostName("smtp.googlemail.com");
+        email.setSmtpPort(465);
+        email.setAuthenticator(new DefaultAuthenticator("podrazad@gmail.com", "tutaj wygeneruj kod aplikacji w google"));
+        email.setSSLOnConnect(true);
+        email.setFrom("podrazad@gmail.com");
+        email.setSubject("TestMail");
+        email.setMsg("This is a test mail ... :-)");
+        email.addTo("podrazad@icloud.com");
+        System.out.println("Rozpoczęto wysyłanie maila ...");
+        email.send();
+        System.out.println("Wysłano");
 
     }
 }
