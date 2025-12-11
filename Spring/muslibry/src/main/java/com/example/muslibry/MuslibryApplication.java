4f9a572f3c5dbe031129b184a89e9f8500f99d52
Dawid <podrazad@gmail.com>
2025-12-11 21:01:19 +0100

    Modified muslibry project

--- Spring/muslibry/src/main/java/com/example/muslibry/MuslibryApplication.java
+++ Spring/muslibry/src/main/java/com/example/muslibry/MuslibryApplication.java
@@ -1,13 +1,17 @@
 package com.example.muslibry;
 
+import com.example.muslibry.model.Song;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class MuslibryApplication {
+public class MuslibryApplication<SongRepository> {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MuslibryApplication.class, args);
+        SpringApplication.run(MuslibryApplication.class, args);
 	}
+    
 
 }
