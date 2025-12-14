453968830bee9e964bf71483dffee8b21c774139
Dawid Podraza <podrazad@gmail.com>
2025-12-14 21:08:23 +0100

    Add KartaOcenFilmow project to repo

---
+++ c#/KartaOcenFilmow/Karta.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KartaOcenFilmow
+{
+    class Karta
+    {
+        //pola
+        List<float> oceny = new List<float>();
+
+        
+
+        //metody
+
+        /// <summary>
+        /// Dodaje nową ocene do listy ocen
+        /// </summary>
+        /// <param name="ocena">nowa ocena</param>
+        public void DodajOcene(float ocena)
+        {
+            oceny.Add(ocena);
+        }
+
+        /// <summary>
+        /// Obliczanie średniej z listy ocen
+        /// </summary>
+        /// <returns>srednia ocena</returns>
+        public float ObliczSrednia()
+        {
+            //float suma = 0;
+            //float srednia = 0;
+            //foreach (var item in oceny)
+            //{
+            //    suma = suma + item;
+            //}
+            //srednia = suma / oceny.Count();
+            //return srednia;
+
+            return oceny.Average();
+        }
+
+        /// <summary>
+        /// Zwraca najmniejszą ocenę
+        /// </summary>
+        /// <returns>najmniejsz ocena</returns>
+        public float NajnizszaOcena()
+        {
+            //float min = float.MaxValue;
+
+            //foreach (var item in oceny)
+            //{
+            //    if (min < item)
+            //    {
+            //        min = item;
+            //    }
+            //}
+            //return min;
+
+            return oceny.Min();
+        }
+
+        /// <summary>
+        /// Zwraca największą ocenę
+        /// </summary>
+        /// <returns>największa ocena</returns>
+        public float NajwiekszaOcena()
+        {
+            //float max = float.MinValue;
+
+            //foreach (var item in oceny)
+            //{
+            //    if (max > item)
+            //    {
+            //        max = item;
+            //    }
+
+            //}
+            //return max;
+            return oceny.Max();
+        }
+
+       
+    }
+}
---
+++ c#/KartaOcenFilmow/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace KartaOcenFilmow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Karta karta = new Karta();
+            
+            karta.DodajOcene(6);
+            karta.DodajOcene(4);
+            karta.DodajOcene(3.3f);
+            karta.DodajOcene(5);
+
+            
+
+            Karta kartaUzytkownika = new Karta();
+
+            
+
+            for (; ; )
+            {
+                Console.WriteLine("Podaj ocene z zakresu 1 - 10");
+                Console.WriteLine("Wyjście wybierz liczbę 666");
+                float ocena;
+                bool wynik= float.TryParse(Console.ReadLine(), out ocena);
+
+                if(!wynik)
+                {
+                    Console.WriteLine("to nie jest liczba");
+                }
+                else if (ocena > 0 && ocena <= 10)
+                {
+                    kartaUzytkownika.DodajOcene(ocena);
+                }
+                else if(ocena == 666)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Liczba z poza zakresu");
+                }
+                
+
+            }
+
+            float srednia = kartaUzytkownika.ObliczSrednia();
+            float max = kartaUzytkownika.NajwiekszaOcena();
+            float min = kartaUzytkownika.NajnizszaOcena();
+
+            Console.WriteLine("Srednia ocena "+ srednia);
+            Console.WriteLine("Majlepsza ocena "+ max);
+            Console.WriteLine("Najgorsza ocena "+ min);
+
+            
+            
+        }
+    }
+    
+}
