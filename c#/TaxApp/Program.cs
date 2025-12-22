89a23a74b49a527a97a1848ab37a48e24982f2d0
Dawid Podraza <podrazad@gmail.com>
2025-12-22 20:39:10 +0100

    Add TaxApp

---
+++ c#/TaxApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TaxApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1= new User();
+            user1.AskForIncome();
+            user1.TaxCount();
+            user1.PrintInfo();
+
+        }
+    }
+}
---
+++ c#/TaxApp/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxApp
+{
+    internal class User
+    {
+        public decimal Income { get; set; }
+
+        public decimal Tax { get; set; }
+
+        public void AskForIncome()
+        {
+            Console.WriteLine("Podaj dochod:");
+            Income = decimal.Parse(Console.ReadLine());
+        }
+
+        public void TaxCount()
+        {
+            if (Income <= 120000)
+            {
+                Tax = (Income * 0.18m) - 556.02m;
+            }
+            else
+            {
+                Tax = 14839.02m + ((Income - 120000m) * 0.32m);
+            }
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine("Podatek do zapaty "+Tax);
+        }
+    }
+}
