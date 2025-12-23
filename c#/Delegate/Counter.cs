6114c0317d509dc3cb54ecbca8caace893495bf1
Dawid Podraza <podrazad@gmail.com>
2025-12-23 20:28:27 +0100

    Delegate exercise in C#

---
+++ c#/Delegate/Counter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    public class Counter
+    {
+       
+        private delegate int NumbersAction(int x,int y);
+
+
+        public void FirstAddThenSubtract(int x,int y)
+        {
+            NumbersAction numbersAction = Add;
+            Console.WriteLine(numbersAction(x,y));
+
+            numbersAction = Subtract;
+            Console.WriteLine(numbersAction(x,y));
+        }
+
+       
+        
+        private static int Add(int x,int y) => x+y;    
+        private static int Subtract(int x,int y) => x-y;
+    }
+}
---
+++ c#/Delegate/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Delegate
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter1 = new Counter();
+
+            counter1.FirstAddThenSubtract(10, 8);
+        }
+    }
+}
