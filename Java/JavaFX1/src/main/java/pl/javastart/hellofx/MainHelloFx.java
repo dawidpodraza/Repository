b5ba5466521b3fa6430b8726ece89449907a82f1
Dawid Podraza <podrazad@gmail.com>
2025-12-29 21:46:03 +0100

    Some trining with Scene Builder JavaFX

--- Java/JavaFX1/src/main/java/pl/javastart/hellofx/MainHelloFx.java
+++ Java/JavaFX1/src/main/java/pl/javastart/hellofx/MainHelloFx.java
@@ -1,16 +1,27 @@
 package pl.javastart.hellofx;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainHelloFx extends Application {
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/fxml/view.fxml")
+        );
+
+        Scene scene = new Scene(loader.load());
+
         stage.setTitle("Hello FX!");
+        stage.setScene(scene);
         stage.show();
     }
 }
